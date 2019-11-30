532a245c669ff2be22c0f812d7b4a36a39a001d2
maximgromov <max9599@gmail.com>
2019-11-30 14:52:47 +0200

    Updates

---
+++ precache-manifest.dd9a4f40d1023d43e8f008190aa9bd36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69c29e20dbd593bad25b21b006e2a8fe",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "e8002a5f302a3ad3c1e9",
+    "url": "/game-of-life/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "f803bd9524c1264b2ceb",
+    "url": "/game-of-life/static/css/main.b21020a8.chunk.css"
+  },
+  {
+    "revision": "e8002a5f302a3ad3c1e9",
+    "url": "/game-of-life/static/js/2.0a71323b.chunk.js"
+  },
+  {
+    "revision": "f803bd9524c1264b2ceb",
+    "url": "/game-of-life/static/js/main.bd6411d5.chunk.js"
+  },
+  {
+    "revision": "b1b93818dc24eabb12bd",
+    "url": "/game-of-life/static/js/runtime-main.17f893ae.js"
+  }
+]);
